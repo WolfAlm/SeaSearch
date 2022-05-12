8d7828f4618ac0f996be1c7dcc8be420d8f4fe4d
Schuyweiz <kirill.pique97@mail.ru>
2022-05-12 21:00:05 +0300

    cors fix

---
+++ src/main/java/space/seasearch/spring/config/WebConfig.java
@@ -0,0 +1,15 @@
+package space.seasearch.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD").allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
